0207f37742fe6a6d4db99366e280e0541c8f5aa2
Mary Jenn <mary@maryfjenn.com>
2013-01-26 13:47:34 -0800

    RailsBridge Intermediate Curriculum, no generators, up to bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogotron::Application.config.secret_token = '439ca4c87fa78099b896b7fbdf5ba43a4ace704de7b2cf21bba2cc75f1dc02e7f5db6c559c4a147330a8e79766bd205d30378ce8238f7e394de544931bfb5155'
